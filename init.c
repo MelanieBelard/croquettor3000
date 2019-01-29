2dd489d4cd80db132ee30b0e79a996e090fa2630
Mélanie <belard.melanie@gmail.com>
2019-01-29 17:29:03 +0100

    First commit

---
+++ init.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Composants
+// IR reciever
+// Potentiometer
+// LED Y
+// LED G
+// LED R
+// Pushbutton
+// Rotation Motor
+// Battery Holder
+// Resistor 1
+// Resistor 2
+// Resistor 3
+// Resistor 4
+
+int main()
+{
+   printf("Hello, World!");
+   return 0;
+}
