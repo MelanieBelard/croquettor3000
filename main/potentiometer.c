1e18ab3b6fa5ec0506e89f544b68d53b473a4d89
JulietteR <juliette@riviere.im>
2019-01-31 12:56:03 +0100

    fix

--- main/potentiometer.c
+++ main/potentiometer.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-#include "esp_system.h"
-#include "driver/gpio.h"
-#include "config.h"
+#include "potentiometer.h"
 
 int getDurationFromPotentiometer(int nPotentiometer){
     int potentiometerValue = GetADC(nPotentiometer);
-	potentiometerToMs = potentiometerValue * 100;
-	duration = pdMS_TO_TICKS(potentiometerToMs);
+	int potentiometerToMs = potentiometerValue * 100;
+	int duration = pdMS_TO_TICKS(potentiometerToMs);
 	printf(duration);
 	return duration;
 }
