11d6e7bdda08fcda41f774a8575a1cce4958ef95
MelanieBelard <belard.melanie@gmail.com>
2019-01-31 14:12:39 +0000

    MAJ potentiometer.c

--- main/potentiometer.c
+++ main/potentiometer.c
@@ -3,9 +3,11 @@
 #include "potentiometer.h"
 
 int getDurationFromPotentiometer(){
-    uint16_t potentiometerValue = adc_read();
+
+  uint16_t potentiometerValue = adc_read();
 	int potentiometerToMs = (unsigned int) potentiometerValue * 100;
 	int duration = pdMS_TO_TICKS(potentiometerToMs);
-	printf("TEST %i \n", duration);
+	printf("Duration: %i \n", duration);
 	return duration;
+  
 }
